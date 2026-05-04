0ddc9143332241b2341b8152539dd9bf176f2160
Radcabna <radcabna@gmail.com>
2026-05-04 19:26:20 +0300

    Initial Commit

---
+++ Fish Habits/Fish_HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fish_HabitsApp.swift
+//  Fish Habits
+//
+//  Created by Алкександр Степанов on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Fish_HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
